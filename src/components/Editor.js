a4efb7ea3dc0bdd50abb1905c4a1c91ce8e3d215
mattchupp <mattlchupp@gmail.com>
2020-06-16 10:20:30 -0500

    changed to class component based on documentation

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,31 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Editor, EditorState} from 'draft-js';
+import {Editor, EditorState, RichUtils} from 'draft-js';
+import 'draft-js/dist/Draft.css';
 
-function TextEditor() {
-  const [editorState, setEditorState] = React.useState(
-    EditorState.createEmpty()
-  );
-
-  const editor = React.useRef(null);
-
-  function focusEditor() {
-    editor.current.focus();
+class TextEditor extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {editorState: EditorState.createEmpty()};
+    this.onChange = editorState => this.setState({editorState});
   }
 
-  React.useEffect(() => {
-    focusEditor()
-  }, []);
 
-  return (
-    <div onClick={focusEditor}>
-      <Editor
-        ref={editor}
-        editorState={editorState}
-        onChange={editorState => setEditorState(editorState)}
-      />
-    </div>
-  );
+  handleKeyCommand(command, editorState) {
+    const newState = RichUtils.handleKeyCommand(editorState, command);
+
+    if (newState) {
+      this.onChange(newState);
+      return 'handled';
+    }
+
+    return 'not-handled';
+  }
+
+  _onBoldClick() {
+    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'BOLD'));
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this._onBoldClick.bind(this)}>Bold</button>
+        <Editor
+          editorState={this.state.editorState}
+          handleKeyCommand={this.handleKeyCommand}
+          onChange={this.onChange}
+        />
+      </div>
+    );
+  }
 }
+
+// ReactDOM.render(<MyEditor />, document.getElementById('container'));
 
 export default TextEditor
